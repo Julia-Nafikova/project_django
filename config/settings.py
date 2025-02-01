f0850ffee4f35a65d4aa61a60cf3181c372eb082
Nafikova_Julia <nafikova91@yandex.ru>
2025-02-01 10:54:55 +0300

    add users app and User model

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',
     'blogs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -95,3 +96,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
